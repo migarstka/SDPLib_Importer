32ede9c1e52bc686a99896fb2ea0bf85f0de48f6
Michael <michael@garstka.org>
2019-09-02 11:08:48 +0100

    Performance improvement
    - Make Fm sparse

--- SDPLib_Importer.jl
+++ SDPLib_Importer.jl
@@ -1,7 +1,7 @@
 # Helper Script that reads in the problems in the SDPA sparese file format and recreates and
-# stores the problem data as matrices in Julia compatible JLD format
+# stores the problem data as matrices in Julia compatible JLD2 format
 
-# The following is stored in the JLD file:
+# The following is stored in the JLD2 file:
 # m: dimension of x
 # n: dimension of Fs,X
 # nblocks: number of blocks in the diagonal structure of the matrices
@@ -12,11 +12,12 @@
 
 # by Michael Garstka
 # University of Oxford, Control Group
-# January 2017
+# January 2018
+# Updated to Julia 1.0 in April 2019
 
 
-workspace()
-using JLD
+
+using FileIO, LinearAlgebra, SparseArrays
 # Specify path to .dat-s files
 dirPath = "./sdplib/"
 
@@ -27,6 +28,11 @@ for f in filter(x -> endswith(x, "dat-s"), readdir(dirPath))
     push!(fileNames,f)
 end
 
+#fileNames = ["equalG11"; "maxG11"; "maxG32"; "mcp124-1"; "mcp250-1"; "mcp500-1"; "qpG11"; "qpG51"; "thetaG11" ]
+# Andersens sparse SDP test problems
+fileNames = ["maxG51"; "maxG55"; "maxG60"; "mcp500-2"; "mcp500-3"; "mcp500-4"; "thetaG51"]# "equalG11" ]
+
+#fileNames = ["qpG11"]
 for file in fileNames
   # open file and read everything into memory
   f = open(dirPath*file*".dat-s");
@@ -57,7 +63,7 @@ for file in fileNames
       if counter == 1
         m = parse(Int64,strip(ln)) + 1
         # now that m is known, initalize the array that holds all the constraint matrices F
-        F = Array{SparseMatrixCSC}(m)
+        F = Array{SparseMatrixCSC}(undef, m)
 
       # nblocks: number of blocks in the diagonal structure of the matrices
       elseif counter == 2
@@ -66,17 +72,17 @@ for file in fileNames
       # vector of numbers that give the sizes on the individual blocks
       # negative number indicates diagonal submatrix
       elseif counter == 3
-      bvec = split(replace(strip(ln,['{','}','(',')']),"+",""))
+      bvec = split(replace(strip(ln,['{','}','(',')']),"+" =>""))
        blockvec = [parse(Float64, ss) for ss in bvec]
        n = Int(sum(abs.(blockvec)))
 
        # objective function vector c
       elseif counter == 4
         # try first to split by whitespace as delimiter (also remove certain trailing, ending characters)
-        cvec = split(replace(strip(ln,['{','}','(',')']),"+",""))
+        cvec = split(replace(strip(ln,['{','}','(',')']),"+" => ""))
         # otherwise try comma as delimiter
         if length(cvec) == 1
-          cvec = split(replace(strip(ln,['{','}','(',')']),"+",""),",")
+          cvec = split(replace(strip(ln,['{','}','(',')']),"+" => ""),",")
         end
         c = [parse(Float64, ss) for ss in cvec]
 
@@ -97,7 +103,7 @@ for file in fileNames
             F[currentM+1] = sparse(Fm)
           end
           # create new temporary matrix that is filled
-          Fm = zeros(n,n)
+          Fm = spzeros(n,n)
           currentM = matno
         end
 
@@ -119,21 +125,21 @@ for file in fileNames
     end
   end
   # after last line has reached save the last F matrix
-  F[currentM+1] = sparse(Fm)
+  F[currentM+1] = Fm
 
   # extract solution for objective value from README file
   f = open(dirPath*"README")
   lines = readlines(f)
   close(f)
   lines = lines[20:113]
-  ln = filter(s -> contains(s,file) ,lines)
+  ln = filter(s -> occursin(file, s) ,lines)
   if length(ln) == 0
     optVal = "Not provided"
   else
     str = split(ln[1])[4]
-    if contains(str,"primal")
+    if occursin("primal", str)
       optVal = Inf
-    elseif contains(str, "dual")
+    elseif occursin("dual", str)
       optVal = -Inf
     else
     optVal = parse(Float64, str)
@@ -141,6 +147,6 @@ for file in fileNames
   end
 
   # save to JLD file
-  JLD.save(dirPath*file*".jld", "m", m-1,"n",n,"nblocks",nblocks,"blockvec",blockvec,"c",c,"F",F,"optVal",optVal)
-  println("Saved problem: $(file) to "*dirPath*file*".jld, Optimal Value: $(optVal)")
+  save(dirPath*file*".jld2", "m", m-1,"n",n,"nblocks",nblocks,"blockvec",blockvec,"c",c,"F",F,"optVal",optVal)
+  println("Saved problem: $(file) to "*dirPath*file*".jld2, Optimal Value: $(optVal)")
 end
--- test.jl
+++ test.jl
@@ -2,29 +2,56 @@
 # Solve problem defined by imported file using ConvexJL and SCS
 # by Michael Garstka
 # University of Oxford, Control Group
-# January 2017
+# January 2018
+# updated in April 2019
 
-workspace()
-using Convex, SCS, JLD
-using Base.Test
 
-# import data from .jld file and create variables
-data = JLD.load("./sdplib/truss1.jld")
+# We work with a semidefinite programming problem that has been written
+# in the following standard form:
+
+# (P)    min c1*x1+c2*x2+...+cm*xm
+#        st  F1*x1+F2*x2+...+Fm*xn-F0=X
+#                                  X >= 0
+
+
+
+using FileIO, COSMO, SparseArrays, LinearAlgebra, Test
+
+# import data from .jld2 file and create variables
+data = load("./sdplib/truss1.jld2");
 F = data["F"]
 c = data["c"]
 m = data["m"]
 n = data["n"]
-optVal = data["optVal"]
+obj_true = data["optVal"]
+
+# Describe primal problem using JuMP and solve with SCS Solver
+# model = JuMP.Model(with_optimizer(COSMO.Optimizer, verbose = true, decompose = true));
+# @variable(model, x[1:m]);
+# @objective(model, Min, c' * x);
+# @constraint(model, con1,  Symmetric(-Matrix(F[1]) + sum(Matrix(F[k + 1]) .* x[k] for k in 1:m))  in JuMP.PSDCone());
+# # JuMP.optimize!(model);
+
+# # ws = backend(model).optimizer.model.optimizer.inner;
+
+# solve with native interface
+
+d = div(n * (n + 1), 2)
+A_man = zeros(d, m)
+# now manually create A and B
+for (i, Fi) in enumerate(F[2:end])
+  t = zeros(d)
+  COSMO.extract_upper_triangle!(Fi, t, sqrt(2))
+  A_man[:, i] = t
+end
+
+b_man = zeros(d)
+COSMO.extract_upper_triangle!(-F[1], b_man, sqrt(2))
 
 
-# Describe problem using ConvexJL and solve with SCS Solver
-x = Variable(m)
-X = Variable(n,n)
-problem = minimize(c'*x)
-constraint1 = sum(F[i+1]*x[i] for i=1:m) - F[1] == X
-constraint2 = isposdef(X)
-problem.constraints += [constraint1,constraint2]
-solve!(problem,SCSSolver(verbose=false))
+cs1 = COSMO.Constraint(A_man, b_man, COSMO.PsdConeTriangle)
+model_direct = COSMO.Model();
+assemble!(model_direct, spzeros(m, m), c, cs1, settings = COSMO.Settings(verbose = true, decompose = false));
+res = COSMO.optimize!(model_direct);
 
-# check that the objective values match
-@test norm(problem.optval - optVal) <= 1e-3
+
